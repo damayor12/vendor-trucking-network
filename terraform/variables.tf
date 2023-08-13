cf52a66567340643720239156f5d2c98334b9d34
Busayo <damayor12@yahoo.com>
2023-08-12 20:12:39 -0400

    added a github actions and terraform config

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {}
+
+variable "app_name" {
+  default = "vendor-network"
+  description = "name of the app"
+}
+
+variable "public_subnets" {
+  default = ["10.10.100.0/24", "10.10.101.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+
+variable "availabilty_zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
